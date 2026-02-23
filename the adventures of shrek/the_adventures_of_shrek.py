18f2caf8b04326d21604a7f980f5d282a624c2bc
Tyler Stimpson <tyler.stimpson@horowhenua.school.nz>
2026-02-24 12:35:53 +1300

    Created monster class

--- the adventures of shrek/the_adventures_of_shrek.py
+++ the adventures of shrek/the_adventures_of_shrek.py
@@ -15,3 +15,11 @@ class Sheild:
         self.defense = defense
     def __str__(self):
         return f"{self.name}: {self.description}. Power: {self.power}"
+
+    class Monster:
+        def __init__(self, name, description, health, attack, defense):
+            self.name = name
+            self.health = health
+            self.attack = attack
+            self.defense = defense
+            self.description = description
