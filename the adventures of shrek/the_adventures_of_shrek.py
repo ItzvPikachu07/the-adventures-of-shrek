78561bae8f6f95af22cf0eca4e8a2a45a477b302
Tyler Stimpson <tyler.stimpson@horowhenua.school.nz>
2026-02-24 12:29:45 +1300

    Created weapon class

--- the adventures of shrek/the_adventures_of_shrek.py
+++ the adventures of shrek/the_adventures_of_shrek.py
@@ -1,1 +1,9 @@
+import random
 
+def __init__(self, name, description, power):
+        self.name = name
+        self.description = description
+        self.power = power
+
+def __str__(self):
+        return f"{self.name}: {self.description} (Power: {self.power})"
